c3794131c3b6e28b4cacc33e44835c981a068d48
Suprita Naik <supritanaik717@gmail.com>
2026-05-10 14:19:10 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxArea(int[] h) {
+        int i = 0,j = h.length-1,max = Integer.MIN_VALUE;
+        while(i<j){
+            max = Math.max(Math.min(h[i],h[j]) * (j-i),max);
+            if(h[i] < h[j]) i++;
+            else j--;
+        }
+        return max;
+    }
+}
